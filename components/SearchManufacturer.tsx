f654546670c0681d7e1842db1efce04035963603
mustafa <msolmaz83@gmail.com>
2023-08-20 12:12:29 +0200

    headlessui

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,11 @@
+import { SearchManuFacturerProps } from '@/types'
+import { Combobox,Transition } from '@headlessui/react'
+import React from 'react'
+
+const SearchManufacturer = ({ manufacturer,setManuFacturer}:SearchManuFacturerProps) => {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
+export default SearchManufacturer
